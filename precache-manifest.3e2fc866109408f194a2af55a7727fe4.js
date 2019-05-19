167edcdbcd618b5e1f54054658ea9a14b745883f
Devvrat Shukla <dshukla@ptc.com>
2019-05-19 19:10:53 +0530

    Updates

---
+++ precache-manifest.3e2fc866109408f194a2af55a7727fe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d94ecedbfd3176b32738",
+    "url": "/react-counter-app/static/js/runtime~main.d94ecedb.js"
+  },
+  {
+    "revision": "682621bcb44ecd35a03c",
+    "url": "/react-counter-app/static/js/main.682621bc.chunk.js"
+  },
+  {
+    "revision": "e497f18dd4da9ce9a9c8",
+    "url": "/react-counter-app/static/js/1.e497f18d.chunk.js"
+  },
+  {
+    "revision": "682621bcb44ecd35a03c",
+    "url": "/react-counter-app/static/css/main.5f4aa27e.chunk.css"
+  },
+  {
+    "revision": "e497f18dd4da9ce9a9c8",
+    "url": "/react-counter-app/static/css/1.8f04e009.chunk.css"
+  },
+  {
+    "revision": "9642fc6bf740919ee61709cce09d8ae6",
+    "url": "/react-counter-app/index.html"
+  }
+];
